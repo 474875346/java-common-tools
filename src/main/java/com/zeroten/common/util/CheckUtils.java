1d6973d9dda7c086924607d4288737cc1afb1bd8
5209691314Aa <474875346@qq.com>
2019-12-11 14:39:16 +0800

    判断可变字符串是否有空字符

--- src/main/java/com/zeroten/common/util/CheckUtils.java
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -1,5 +1,15 @@
 package com.zeroten.common.util;
 
 public class CheckUtils {
-
+    /*
+     * 如果Strings中有任意一个字符串为空，则返回false，否则返回true。
+     * */
+    public static boolean isAnyEmpty(String...strings){
+        for (int a = 0;a<strings.length;a++){
+            if (strings[a].equals("")){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -1,4 +1,12 @@
 package com.zeroten.common.util;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CheckUtilsTest {
+    @Test
+    public void testisAnyEmpty() {
+        Assert.assertEquals(CheckUtils.isAnyEmpty("11","","123123","  123","  "),false);
+        Assert.assertEquals(CheckUtils.isAnyEmpty("11","12312","123123","  123","  "),true);
+    }
 }
