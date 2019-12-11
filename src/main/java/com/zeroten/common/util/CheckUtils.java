9ef7a842f4c2f7b4bbf3692210c64919092b6fde
5209691314Aa <474875346@qq.com>
2019-12-11 20:08:37 +0800

    判断 str1与str2 字符串是否相等

--- src/main/java/com/zeroten/common/util/CheckUtils.java
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -21,4 +21,11 @@ public class CheckUtils {
         }
         return false;
     }
+
+    public static boolean equals(String s1, String s2) {
+        if (s1 == null || s2 == null || !s1.equals(s2)){
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -7,16 +7,26 @@ public class CheckUtilsTest {
 
     @Test
     public void testisAnyEmpty() {
-        Assert.assertEquals(CheckUtils.isAnyEmpty("11","","123123","  123","  "),false);
-        Assert.assertEquals(CheckUtils.isAnyEmpty("11","12312","123123","  123","  "),true);
+        Assert.assertEquals(CheckUtils.isAnyEmpty("11", "", "123123", "  123", "  "), false);
+        Assert.assertEquals(CheckUtils.isAnyEmpty("11", "12312", "123123", "  123", "  "), true);
     }
 
     @Test
     public void testisEmpty() {
-        Assert.assertEquals(CheckUtils.isEmpty(null),true);
+        Assert.assertEquals(CheckUtils.isEmpty(null), true);
         String[] arr = {};
-        Assert.assertEquals(CheckUtils.isEmpty(arr),true);
-        String[] arr1 = {"1","2"};
-        Assert.assertEquals(CheckUtils.isEmpty(arr1),true);
+        Assert.assertEquals(CheckUtils.isEmpty(arr), true);
+        String[] arr1 = {"1", "2"};
+        Assert.assertEquals(CheckUtils.isEmpty(arr1), true);
     }
+
+    @Test
+    public void testequals() {
+        Assert.assertEquals(CheckUtils.equals("1","2"),false);
+        Assert.assertEquals(CheckUtils.equals("2","2"),true);
+        Assert.assertEquals(CheckUtils.equals(null,"2"),false);
+        Assert.assertEquals(CheckUtils.equals("1",null),false);
+    }
+
+
 }
