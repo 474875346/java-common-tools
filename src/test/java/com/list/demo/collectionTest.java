b313ec2d8b44bebd13fdc2b593a64c2d179f9fde
烟汐忆梦 <474875346@qq.com>
2020-01-06 14:42:28 +0800

    冒泡排序/插入排序/选择排序修改

---
+++ src/test/java/com/list/demo/collectionTest.java
@@ -0,0 +1,14 @@
+package com.list.demo;
+
+
+public class collectionTest {
+    public static void main(String[] args) {
+        System.out.println("冒泡排序:");
+        collection.BubbleSort();
+        System.out.println("插入排序:");
+        collection.InsertionSort();
+        System.out.println("选择排序:");
+        collection.SelectionSort();
+    }
+
+}
